c7095b25d6e8511e28e840a58b27b9845a0138df
deezolut <deezolut@gmail.com>
2024-09-17 15:49:47 +0300

    Create function that returns 1 of 3 values

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+// Create function that randomly returns: “rock”, “paper” or “scissors”.
+function getComputerChoice() {
+    let randomChoice = Math.random();
+    if (randomChoice <= 0.33) {
+        return "rock"
+    }
+    else if (randomChoice > 0.33 && randomChoice <= 0.66) {
+        return "paper"
+    }
+    else if (randomChoice > 0.66 && randomChoice <= 1) {
+        return "scissors"
+    }
+}
